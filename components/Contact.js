de9df81f694c9c7bbeec39b1b058b04562e60603
Rafael Laube <laube.developer@gmail.com>
2023-11-30 23:45:04 -0300

    erros deploy 8

--- components/Contact.js
+++ components/Contact.js
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function Contact({name, description, hour, img_url}){
     return (<div className={styles.contact}>
-        <Image src={img_url} alt="user image" width={200} height={200}/>
+        <Image src={img_url} alt="user image" width={30} height={30}/>
         <div className={styles.bottom}>
             <div>
                 <h3>{name}</h3>

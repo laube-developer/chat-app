5c5be844360e8f2cc13ad29b2468a303491b10ea
Rafael Laube <laube.developer@gmail.com>
2023-11-30 23:43:36 -0300

    erros deploy 7

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'avatars.githubusercontent.com/u/',
+        hostname: 'avatars.githubusercontent.com/u',
         port: '',
         pathname: '/**',
       },
